d7298fa863cabeb6a6dc135643a081d8289425c3
Kishore <kishore22705@gmail.com>
2024-08-03 23:00:59 +0530

    text

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler:{
-        styledComponents:true,
-    }
-};
-
-export default nextConfig;
+    // output: 'undefined',
+    //   distDir: 'out',
+    };
+    
+    export default nextConfig;
+    
