193ce808b81af708c608e3a97126e8919c16e396
Kishore <kishore22705@gmail.com>
2024-08-05 11:09:42 +0530

    .

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,12 +1,12 @@
 'use client';
-import { useCartContext } from '@/components/CartContext';
+// import { useCartContext } from '@/components/CartContext';
 const page = () => {
     const { cartProducts, setCartProducts } = useCartContext();
   return (
     <main className='h-screen w-screen bg-gray-100'>
         <div className="bg-white ">
             <h1 className='font-bold text-2xl'>Cart</h1>
-            {cartProducts}
+            {/* {cartProducts} */}
         </div>
         <div className="bg-white">hi</div>
     </main>
